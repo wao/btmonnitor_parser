5c3f98235dc031a0d2ccb831fbdba2c4d6aed07d
Chen Yang <yangchen@thinkmore.info>
2022-09-25 10:40:52 -0400

    support iso data

--- rttlog.py
+++ rttlog.py
@@ -7,13 +7,53 @@ from datetime import datetime
 from btmonitor_parser.parser import Parser
 import socket
 from btmonitor_parser import parser
+import multiprocessing as mp
+from loguru import logger
 
-UDP_Stream = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+def do_parse(udp_stream, p ,data):
+    pkt = p.next(data)
+    while pkt is not None:
+        if pkt.is_btsnoop():
+            #print(pkt.to_btsnoop())
+            #print(''.join(['\\x%02x' % b for b in pkt.to_ellisys()]))
+            udp_stream.sendto(pkt.to_ellisys(), ( "192.168.0.199", 24352 ) )
+
+        elif pkt.opcode == parser.BT_MONITOR_ISO_TX_PKT:
+            #TODO write log iso later
+            pass
+        elif pkt.opcode == parser.BT_MONITOR_NEW_INDEX:
+            #TODO write log iso later
+            pass
+        elif pkt.opcode == parser.BT_MONITOR_OPEN_INDEX:
+            #TODO write log iso later
+            pass
+        else:
+            logger.warning(f"Unhandled packet {pkt}")
+
+        pkt = p.next()
+
+
+def parser_proc(q):
+    logger.info( "Parser Process started" )
+    udp_stream = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+
+    p = Parser()
+    
+    while True:
+        if not p.is_top_buffer_empty():
+            print("Reset parser")
+            p = Parser()
+
+        data = q.get()
+        do_parse(udp_stream, p, data)
+
+    logger.info( "Parser Process Exited" )
 
 class JLinkSession:
-    def __init__(self):
+    def __init__(self,queue):
         self.client = None
         self.jlink = pylink.JLink()
+        self.queue = queue
 
     def set_client(self,clt):
         self.client = clt
@@ -37,9 +77,6 @@ class JLinkSession:
 #writedata = input()
 #jlink.rtt_write(0, [ord(x) for x in list(writedata)])
 
-        self.parser = Parser()
-
-
         while True:
             try:
                 print( f"up {self.jlink.rtt_get_num_up_buffers()}" )
@@ -53,15 +90,15 @@ class JLinkSession:
             while self.running:
                 readdata = self.jlink.rtt_read(1, 1024)
                 if len(readdata) == 0:
-                    time.sleep(0.1)
+                    time.sleep(0.001)
                 else:
                     readdata=bytes(readdata)
-                    #print( f"Read data len {len(readdata)} {type(readdata[0])}" )
+                    print( f"Read data len {len(readdata)} {type(readdata[0])}" )
                     #print(''.join(['\\x%02x' % b for b in readdata]))
                     #wr.write(bytes(readdata))
                     #wr.flush()
+                    self.queue.put(readdata)
 
-                    self.do_parse(readdata)
         except Exception:
             print( "Got key in tread, exit it" )
 
@@ -74,33 +111,6 @@ class JLinkSession:
     def interrput(self):
         self.running = False
 
-    def do_parse(self,data):
-        self.parser = Parser()
-        pkt = self.parser.next(data)
-        while pkt is not None:
-            if pkt.is_btsnoop():
-                #print(pkt.to_btsnoop())
-                print(''.join(['\\x%02x' % b for b in pkt.to_ellisys()]))
-                UDP_Stream.sendto(pkt.to_ellisys(), ( "192.168.0.199", 24352 ) )
-                if self.client:
-                    try:
-                        self.client.send(pkt.to_btsnoop())
-                    except Exception:
-                        self.client = None
-
-            elif pkt.opcode == parser.BT_MONITOR_ISO_TX_PKT:
-                #TODO write log iso later
-                pass
-            elif pkt.opcode == parser.BT_MONITOR_NEW_INDEX:
-                #TODO write log iso later
-                pass
-            elif pkt.opcode == parser.BT_MONITOR_OPEN_INDEX:
-                #TODO write log iso later
-                pass
-            else:
-                self.logger.warning(f"Unhandled packet {pkt}")
-
-            pkt = self.parser.next()
 
 BTSNOOP_FILE_HDR=bytes([ 0x62, 0x74, 0x73, 0x6e, 0x6f, 0x6f, 0x70, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x03, 0xea ])
 
@@ -126,11 +136,15 @@ def rtt_thread_proc(j):
         j.stop()
         time.sleep(2)
 
-js = JLinkSession()
+DataQueue = mp.Queue()
 
+js = JLinkSession(DataQueue)
 
 #_thread.start_new_thread(fwd_proc, (js,))
 _thread.start_new_thread(rtt_thread_proc, ( js, ))
+
+parser_process = mp.Process(target=parser_proc, args=(DataQueue,))
+parser_process.start()
 
 while True:
     input()
